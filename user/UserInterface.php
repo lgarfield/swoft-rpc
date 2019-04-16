7105d000a9db4969850ef4ce3ef3e63d11a8b133
lgarfield <li.nextcit@gmail.com>
2019-04-16 23:59:43 +0800

    add user interface file.

--- user/UserInterface.php
+++ user/UserInterface.php
@@ -1,2 +1,34 @@
 <?php
+/**
+ * user interface
+ *
+ * @author garfield.li
+ * @time 2019/04/16 11:16pm
+ */
+namespace SwoftRpc\User;
 
+use Swoft\Core\ResultInterface;
+
+/**
+ * The interface of user service.
+ *
+ * @method ResultInterface login(string $mobile)
+ * @method ResultInterface loginThird(string $token)
+ *
+ */
+class UserInterface
+{
+    /**
+     * @param string $mobile
+     *
+     * @return array
+     */
+    public function login(string $mobile): array;
+
+    /**
+     * @param string $token
+     *
+     * @return array
+     */
+    public function loginThird(string $token): array;
+}
