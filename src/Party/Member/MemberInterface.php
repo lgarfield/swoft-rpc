d103f7e424cca7b7860fe18340e9734ee3b7e4c7
lazy <lidan729011771@gmail.com>
2019-05-10 14:27:07 +0800

    Add get UserInfo and UserOrg

--- src/Party/Member/MemberInterface.php
+++ src/Party/Member/MemberInterface.php
@@ -25,4 +25,15 @@ interface MemberInterface
      * @return array
      */
     public function getUserInfo(int $id, array $field): array;
+
+
+    /**
+     * 获取党员组织信息
+     *
+     * User: lazy
+     * Date: 2019-05-10 14:17
+     * @param array $ids
+     * @return array
+     */
+    public function getUserOrg(array $ids): array;
 }
